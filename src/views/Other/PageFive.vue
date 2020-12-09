8ecfbbabf34df9b4baf2e42edbc84901bd18ffc6
msd673 <2309721596@qq.com>
2020-12-09 23:59:30 +0800

    exportUser

--- src/views/Other/PageFive.vue
+++ src/views/Other/PageFive.vue
@@ -40,7 +40,7 @@
             <div>
                 <el-button type="primary" @click="addUser">新增</el-button>
                 <el-button type="primary" @click="delUser">删除</el-button>
-                <el-button type="primary" @click="delUser">导出</el-button>
+                <el-button type="primary" @click="exportUser">导出</el-button>
             </div>
 
             <common-form inline :formLabel="formLabel" :form="searchFrom">
@@ -56,6 +56,8 @@
                 @changePage="getList()"
                 @edit="editUser"
                 @del="delUser"
+                @export="exportUser"
+                id="out-table"
         ></common-table>
     </div>
 </template>
@@ -63,6 +65,8 @@
 <script>
     import CommonForm from "../../components/CommonForm";
     import CommonTable from "../../components/CommonTable";
+    import FileSaver from "file-saver";
+    import XLSX from "xlsx";   
     import axios from '../../axios/ajax'
     import qs from 'qs'
     export default {
@@ -319,7 +323,32 @@
                             message: "已取消删除"
                         });
                     });
-            }
+            },
+    //定义导出Excel表格事件
+    exportUser() {
+      /* 从表生成工作簿对象 */
+      var wb = XLSX.utils.table_to_book(document.querySelector("#out-table"));
+      /* 获取二进制字符串作为输出 */
+      var wbout = XLSX.write(wb, {
+        bookType: "xlsx",
+        bookSST: true,
+        type: "array"
+      });
+      try {
+        FileSaver.saveAs(
+          //Blob 对象表示一个不可变、原始数据的类文件对象。
+          //Blob 表示的不一定是JavaScript原生格式的数据。
+          //File 接口基于Blob，继承了 blob 的功能并将其扩展使其支持用户系统上的文件。
+          //返回一个新创建的 Blob 对象，其内容由参数中给定的数组串联组成。
+          new Blob([wbout], { type: "application/octet-stream" }),
+          //设置导出文件名称
+          "测试文档.xlsx"
+        );
+      } catch (e) {
+        if (typeof console !== "undefined") console.log(e, wbout);
+      }
+      return wbout;
+    }
         },
         created () {
             this.getList();
